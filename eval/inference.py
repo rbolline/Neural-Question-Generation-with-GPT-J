fca6c6e6465fc36d2cb6df1180073ef41fcb1b1d
sukritrao <sukritrao@gmail.com>
2022-04-18 20:45:41 -0400

    updated prompt creation logic for question generation

--- eval/inference.py
+++ eval/inference.py
@@ -17,6 +17,8 @@ sys.path.append(os.path.abspath("../"))
 import argparse
 import pandas as pd
 from tqdm import tqdm
+import yaml
+import pickle
 
 import torch
 from transformers import AutoTokenizer, GPTJForCausalLM, AutoModelForCausalLM
@@ -42,7 +44,7 @@ def load_model(use_opt_model=True):
     return model
 
 
-def load_dataset(num_examples, is_train, batch_size):
+def load_dataset(config):
     race_dataset = RaceDataset()
 
     test_df = race_dataset.get_split('test', do_preprocess=True)
@@ -53,18 +55,21 @@ def load_dataset(num_examples, is_train, batch_size):
     sample_test_df = test_df[test_df.groupby('example_id').ngroup() < 2]
 
     # create set of prompts for question generation
-    prompt_collection = race_dataset.prepare_data_qg(sample_test_df, num_examples, is_train)
+    prompt_df = race_dataset.prepare_data_qg(sample_test_df,
+                                             config['num_examples'],
+                                             config['is_train'])
 
     # batch the inputs for infernce
-    if batch_size == 1:
-        batch_prompts = [[prompt] for prompt in prompt_collection]
-    else:
-        batch_prompts = [prompt_collection[idx : idx + batch_size] for idx in range(0, len(prompt_collection), batch_size)]
+    batch_size = config['batch_size']
+    batch_prompts = [prompt_df.iloc[idx : idx + batch_size] for idx in range(0, len(prompt_df), batch_size)]
 
     return batch_prompts
 
 
-def get_model_gen_text(model, tokenizer, prompts):
+def get_model_gen_text(model,
+                       tokenizer,
+                       batched_prompt_df,
+                       config):
     """Generates text using model and prompt"""
     # if cuda exists, use cuda, else run on cpu
     if torch.cuda.is_available():
@@ -74,40 +79,33 @@ def get_model_gen_text(model, tokenizer, prompts):
 
     model.to(device)
 
+    model_params = config['model_params']
     results = []
-    for batch_prompts in tqdm(prompts):
+    for batch_df in tqdm(batched_prompt_df):
+        batch_prompts = batch_df['prompt'].tolist()
+        print(batch_prompts)
+
         tokenized_inputs = tokenizer(batch_prompts,
                                      return_tensors="pt",
                                      padding=True,
                                      truncation=True).to(device)
         input_ids = tokenized_inputs.input_ids
 
-        gen_tokens = model.generate(input_ids,
-                                    do_sample=True,
-                                    temperature=0.3,
-                                    max_length=1000,)
+        print(input_ids)
 
-        gen_text = tokenizer.batch_decode(gen_tokens)[0]
+        gen_tokens = model.generate(input_ids, **model_params)
+        gen_text = tokenizer.batch_decode(gen_tokens)
 
-        results.append("".join(gen_text))
+        batch_df['gen_text'] = gen_text
+        results.append(batch_df)
 
     return results
 
 
-def main(args):
+def main(config):
     """Defines main execution"""
-    if args.use_opt_model == "True":
-        use_opt_model = True
-    else:
-        use_opt_model = False
-
-    if args.is_train == "True":
-        is_train = True
-    else:
-        is_train = False
-
     # load the GPT-J model
-    model = load_model(use_opt_model)
+    model = load_model(config['use_opt_model'])
 
     print("**** FINISHED LOADING MODEL!! *******")
 
@@ -122,41 +120,36 @@ def main(args):
     print("**** FINISHED LOADING TOKENIZER!! *******")
 
     # prepare the dataset of prompts
-    batched_prompts = load_dataset(args.num_examples, is_train, int(args.batch_size))
+    batched_prompt_df = load_dataset(config)
 
     print("**** FINISHED LOADING DATSET!! *******")
-    print(batched_prompts[0])
+    print(batched_prompt_df[0].head(2))
 
     # gen text from the model
-    gen_text = get_model_gen_text(model, tokenizer, batched_prompts)
+    gen_text = get_model_gen_text(model, tokenizer, batched_prompt_df, config)
 
     return gen_text
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Arguments for BLEU score computation")
-    parser.add_argument('--use_opt_model',
-                        help='''if optimized GPT-J model should be used for
-                            inference. Includes fp16 precision''',
-                        default="True",
-                        choices=['True', 'False'],
-                        required=False)
-    parser.add_argument('--is_train', help='''training or inference mode''',
-                        default="False",
-                        choices=['True', 'False'],
-                        required=False)
-    parser.add_argument('--num_examples',
-                        help='''num of example questions to use in
-                            each prompt''',
-                        default=1,
-                        required=False)
-    parser.add_argument('--batch_size',
-                        help='''number of examples per batch during inference''',
-                        default=2,
+    parser.add_argument('--config',
+                        help='''path to config file''',
+                        default="./inference_config.yaml",
                         required=False)
     args = parser.parse_args()
 
-    gen_text = main(args)
+    with open(args.config, 'r') as yml_file:
+        try:
+            config = yaml.safe_load(yml_file)
+        except yaml.YAMLError as exc:
+            raise(exc)
+
+    results = main(config)
 
     # TODO: change this later. save results
-    pd.DataFrame(gen_text).to_csv("./sample_gen_text_output.csv", index=False, header=True)
+    with open(r'./sample_output.pickle', 'wb') as fh:
+        pickle.dump(results, fh)
+
+    results_df = pd.concat(results, axis=0)
+    results_df.to_csv(config['savepath'], index=False, header=True)
