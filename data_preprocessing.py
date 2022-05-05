045192b703da7862cb53b8b17a57cbc7e888531a
sukritrao <sukritrao@gmail.com>
2022-05-04 20:57:42 -0400

    made changes to data preprocessing pipeline

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,8 +4,9 @@ import string
 
 import torch
 from torch.utils.data import Dataset
-from util_methods import encode_data
 import pandas as pd
+import yaml
+import argparse
 
 from datasets import load_dataset
 
@@ -46,20 +47,20 @@ class RaceDataset(Dataset):
         Args:
             df_split: pd.DataFrame
         """
-        df_split['answer'] = df_split['answer'].map({'A':0, 'B':1, 'C':2, 'D':3})
-        df_split['answer'] = df_split.apply(lambda row: row['options'][row['answer']], axis=1)
-        df_split['answer'] = df_split['answer'].str.strip('.')
+        df_split.loc[:, 'answer'] = df_split['answer'].map({'A':0, 'B':1, 'C':2, 'D':3})
+        df_split.loc[:, 'answer'] = df_split.apply(lambda row: row['options'][row['answer']], axis=1)
+        df_split.loc[:, 'answer'] = df_split['answer'].str.strip('.')
 
-        df_split['article'] = df_split['article'].str.strip('.')
+        df_split.loc[:, 'article'] = df_split['article'].str.strip('.')
 
-        df_split['difficulty_label'] = np.where(df_split['example_id'].str.contains("high"), "Hard", "Easy")
+        df_split.loc[:, 'difficulty_label'] = np.where(df_split['example_id'].str.contains("high"), "Hard", "Easy")
 
         # store the id of each context passage
-        df_split['context_id'] = df_split['example_id']
+        df_split.loc[:, 'context_id'] = df_split['example_id']
 
         # create unique ids for each context question pair
-        df_split['rolling_count'] = df_split.groupby((df_split['example_id'] != df_split['example_id'].shift(1)).astype(int).cumsum()).cumcount()
-        df_split['example_id'] = df_split['example_id'].str.split(".").str[0] + "_" + df_split['rolling_count'].astype(str)
+        df_split.loc[:, 'rolling_count'] = df_split.groupby((df_split['example_id'] != df_split['example_id'].shift(1)).astype(int).cumsum()).cumcount()
+        df_split.loc[:, 'example_id'] = df_split['example_id'].str.split(".").str[0] + "_" + df_split['rolling_count'].astype(str)
 
         return df_split
 
@@ -70,18 +71,18 @@ class RaceDataset(Dataset):
         # df_split = self.drop_numeric(df_split)
 
         ## Rule 2: Exclude phrase completion questions
-        df_split = df_split[ ~df_split['question'].str.contains('_')]
+        # df_split = df_split[ ~df_split['question'].str.contains('_')]
 
         ## Rule 3: Exclude "According to the passage" questions
-        df_split = df_split[ ~df_split['question'].str.contains('According to the passage')]
+        # df_split = df_split[ ~df_split['question'].str.contains('According to the passage')]
 
-        ## Rule 4: Exclude questions shorter than 5 words
-        df_split = df_split[ df_split.question.str.replace(',','').str.split().str.len() > 5 ]
+        ## Rule 4: Exclude answers shorter than 5 words
+        df_split = df_split.loc[ df_split['answer'].str.split().str.len() > 3 ]
 
         ## TODO: add flag for zero-shot
-        ## Drop passages that have just a single question left after filtering
+        ## Drop passages that have just less than 3 questions left after filtering
         context_count_df = df_split.groupby("context_id").size().to_frame('count').reset_index()
-        context_ids_to_drop = context_count_df.loc[context_count_df['count'] < 2, 'context_id'].tolist()
+        context_ids_to_drop = context_count_df.loc[context_count_df['count'] < 4, 'context_id'].tolist()
 
         df_split = df_split.loc[ ~df_split['context_id'].isin(context_ids_to_drop)]
 
@@ -118,26 +119,37 @@ class RaceDataset(Dataset):
 
                 if is_train:
                     # if in training mode then include the question for all examples
-                    include_df['prompt'] = include_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question: {row['question']}", axis=1)
-                    example_df['prompt'] = example_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question: {row['question']}", axis=1)
+                    include_df.loc[:, 'prompt'] = include_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question: {row['question']}", axis=1)
+                    example_df.loc[:, 'prompt'] = example_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question: {row['question']}", axis=1)
                 else:
                     # if in test mode then include the questions for all examples except the test example.
-                    include_df['prompt'] = include_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question: {row['question']}", axis=1)
-                    example_df['prompt'] = example_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question:", axis=1)
-
-                num_examples = min(len(group) - 1, num_examples)
-                context = group['article'].iloc[0]
+                    include_df.loc[:, 'prompt'] = include_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question: {row['question']}", axis=1)
+                    example_df.loc[:, 'prompt'] = example_df.apply(lambda row: f"Difficulty: {row['difficulty_label']}. Answer: {row['answer']}. Question:", axis=1)
 
                 # include the questions from all other examples for the given context
-                prompt = "\n".join(include_df.sample(num_examples, replace=False)['prompt'].tolist())
+                # first include questions that are not repeated
+                num_unique_questions = min(len(include_df), num_examples)
+                prompt_examples = include_df.sample(num_unique_questions, replace=False)['prompt'].tolist()
+
+                try:
+                    if num_unique_questions < num_examples:
+                        diff = num_examples - num_unique_questions
+                        num_rep_questions = include_df.sample(diff, replace=False)['prompt'].tolist()
+                        prompt_examples += num_rep_questions
+                except ValueError as err:
+                    raise err
+                    # print(f"num_unique_questions={num_unique_questions} num_examples={num_examples} {num_unique_questions < num_examples}, diff={diff}")
+
+                prompt = "\n".join(prompt_examples)
 
                 # include the question for the given example
                 prompt = f"{prompt}\n{example_df['prompt'].iloc[0]}"
 
                 # add the context article to the prompt
+                context = group['article'].iloc[0]
                 prompt = f"{context}\n{prompt}"
 
-                example_df['prompt'] = prompt
+                example_df.loc[:, 'prompt'] = prompt
                 prompt_collection.append(example_df)
 
             return prompt_collection
@@ -146,12 +158,47 @@ class RaceDataset(Dataset):
         for _, group in df_split.groupby('context_id'):
             prompt_collection += create_prompt(group, num_examples, is_train)
 
+
         prompt_df = pd.concat(prompt_collection, axis=0)
         prompt_df = prompt_df.sort_values('prompt')
         self.dataset = prompt_df
-        # batch_prompts = [prompt_df.iloc[idx : idx + batch_size] for idx in range(0, len(prompt_df), batch_size)]
-        # self.dataset = batch_prompts
 
     def collate_fn(self, batch):
         batch_df = pd.concat(batch, axis=0)
         return batch_df.to_dict('list')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Arguments for BLEU score computation")
+    parser.add_argument('--config',
+                        help='''path to config file''',
+                        default="./eval/inference_config.yaml",
+                        required=False)
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as yml_file:
+        try:
+            config = yaml.safe_load(yml_file)
+        except yaml.YAMLError as exc:
+            raise(exc)
+
+    race_dataset = RaceDataset()
+
+    dataset = race_dataset.get_split(config['data_split'], do_preprocess=True)
+
+    ##TODO: Included only for testing. remove later
+    #sample_test_df = test_df[test_df.groupby('context_id').ngroup() < 2]
+
+    # create set of prompts for question generation
+    race_dataset.prepare_data_qg(dataset,
+                                 config['num_examples'],
+                                 config['is_train'])
+
+    data_loader = torch.utils.data.DataLoader(dataset=race_dataset,
+                                              batch_size=config['batch_size'],
+                                              collate_fn=race_dataset.collate_fn,
+                                              shuffle=False)
+
+    print(f"LENGTH OF DATASET: {len(race_dataset)}")
+    print(iter(data_loader).next()['prompt'][0])
+    print(race_dataset.dataset['difficulty_label'].value_counts())
